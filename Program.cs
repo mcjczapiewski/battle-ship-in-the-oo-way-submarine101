62af818e812941e69d8b2b8b8603ed16a7c0c822
Pati <ppeczynska@gmail.com>
2020-05-24 21:08:25 +0200

    add classes

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace battle_ship_in_the_oo_way_submarine101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
