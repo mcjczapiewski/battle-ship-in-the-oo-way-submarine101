3d86c10795a5f8c9fa31958e2b7d2c3afe1049a9
Pati <ppeczynska@gmail.com>
2020-05-28 13:05:16 +0200

    some first tries with Ocean classes Myboard EnemyBoard

--- OCEAN/Ocean.cs
+++ OCEAN/Ocean.cs
@@ -1,9 +1,33 @@
-using Ship
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using DefaultNamespace.Square
 
-namespace DefaultNamespace
+namespace battle_ship_in_the_oo_way_submarine101.OCEAN
 {
-    public class Ocean
+    public abstract class Ocean
     {
-        
+        public int length, hight;
+        public string name;
+
+        public Ocean(int length, int hight, string name)
+        {
+            this.length = length;
+            this.hight = hight;
+            this.name = "setMyName";
+        } 
+    public class MyBoard() : Ocean /// MyBoard that inheritate from Ocean///
+    {
+        public MyBoard(int length, int hight, string name) : base(length, hight, name)
+        {
+            length = 5;
+            hight = 5;
+            name = "This is your board";
+        }
+    }
+
+    public class EnemyBoard() : Ocean
+    {
+        public EnemyBoard()
     }
 }
--- SHIP/Cruiser.cs
+++ SHIP/Cruiser.cs
@@ -3,7 +3,7 @@ using System;
 
 namespace battle_ship_in_the_oo_way_submarine101
 {
-	public class Cruiser 
+	public class Cruiser : Ship
 	{
 		public Cruiser()
 		{
