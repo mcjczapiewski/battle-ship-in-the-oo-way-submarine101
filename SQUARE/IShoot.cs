d319dbf86971888b5164addb4c63d821e9f305c4
Maciej Czapiewski <mcjczapiewski@gmail.com>
2020-06-05 13:34:39 +0200

    Further fails

--- SQUARE/IShoot.cs
+++ SQUARE/IShoot.cs
@@ -1,36 +1,41 @@
-﻿namespace battle_ship_in_the_oo_way_submarine101.SQUARE
+﻿using battle_ship_in_the_oo_way_submarine101.OCEAN;
+using static battle_ship_in_the_oo_way_submarine101.OCEAN.Ocean;
+
+namespace battle_ship_in_the_oo_way_submarine101.SQUARE
 {
-    public abstract class IShoot
+    public interface IShoot
     {
         /// <summary>
         /// Check what type of square is this
         /// </summary>
-        public abstract void CheckOccupation();
+        void CheckOccupation();
 
         /// <summary>
         /// Change the symbol of square in my array
         /// </summary>
         /// <param name="Row">from user input</param>
         /// <param name="Column">from user input</param>
-        public void SetProperMark(int Row, int Column)
+        void SetProperMark(int Row, int Column)
         {
+            Ocean myBoard = new MyBoard();
+            Ocean enemyBoard = new EnemyBoard();
             // check at opponents board square type from array
-            var whatItShouldBe = OCEAN.Ocean.EnemyBoard.fields[Row, Column];
+            var whatItShouldBe = enemyBoard.fields[Row, Column];
             // change mark at my board to the mark of opponent
-            var isItOccupied = new Square.IsOccupied;
+            Square isItOccupied = new Square.IsOccupied();
             if (whatItShouldBe == OccupationType.Empty)
             {
-                OCEAN.Ocean.MyBoard.fields[Row, Column] = OccupationType.Miss;
+                myBoard.fields[Row, Column] = OccupationType.Miss;
             }
             else if (isItOccupied)
             {
-                OCEAN.Ocean.MyBoard.fields[Row, Column] = OccupationType.Hit;
+                myBoard.fields[Row, Column] = OccupationType.Hit;
             }
         }
 
         /// <summary>
         /// If the square is a ship, hit it
         /// </summary>
-        public abstract void HitThatShip();
+        void HitThatShip();
     }
 }
