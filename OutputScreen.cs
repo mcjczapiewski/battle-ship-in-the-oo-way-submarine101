a737695d8e1f59e4c7fc93fa323b7173348f72c2
igorn <i.nowacki89@gmail.com>
2020-06-11 12:22:12 +0200

    Create intro screen

---
+++ OutputScreen.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace battle_ship_in_the_oo_way_submarine101
+{
+    class OutputScreen
+    {
+        public static void ShowIntroScreen()
+        {
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("#######################################");
+            Console.Write("#");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("        Welcome to Batttleship!      ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("#");
+            Console.WriteLine("#######################################");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        public static void Header()
+        {
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("#######################################");
+            Console.Write("#");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("             Batttleship!            ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("#");
+            Console.WriteLine("#######################################");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("");
+        }
+    }
+}
