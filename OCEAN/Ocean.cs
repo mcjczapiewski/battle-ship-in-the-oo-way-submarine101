aa79a224f19128abc299d8d83244ef3ac0b74763
igorn <i.nowacki89@gmail.com>
2020-06-11 13:59:43 +0200

    Create board printing without square classes

--- OCEAN/Ocean.cs
+++ OCEAN/Ocean.cs
@@ -56,7 +56,89 @@ namespace battle_ship_in_the_oo_way_submarine101.OCEAN
                         Console.Write(Ship.Name);
             }
         }
+        static string NumberToLetter(int number)
+        {
+            string result = "";
+            switch (number)
+            {
+                case 1:
+                    result = "A";
+                    break;
+                case 2:
+                    result = "B";
+                    break;
+                case 3:
+                    result = "C";
+                    break;
+                case 4:
+                    result = "D";
+                    break;
+                case 5:
+                    result = "E";
+                    break;
+                case 6:
+                    result = "F";
+                    break;
+                case 7:
+                    result = "G";
+                    break;
+                case 8:
+                    result = "H";
+                    break;
+                case 9:
+                    result = "I";
+                    break;
+                case 10:
+                    result = "J";
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+        public static void DrawBoard()
+        {
+            Console.WriteLine("Your Board:                              Firing Board:");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("  ");
+            for (int y = 1; y <= 10; y++)
+            {
+                Console.Write(y);
+                Console.Write(" ");
+            }
+            Console.Write("                   ");
+            for (int y = 1; y <= 10; y++)
+            {
+                Console.Write(" ");
+                Console.Write(y);
+            }
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.White;
+            for (int x = 1; x <=10; x++)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(NumberToLetter(x));
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("|");
+                for (int y = 1; y <=10; y++)
+                {
+                    Console.Write(" ");
+                    Console.Write("|");
+                }
+                Console.Write("                   ");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(NumberToLetter(x));
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("|");
+                for (int y = 1; y <= 10; y++)
+                {
+                    Console.Write(" ");
+                    Console.Write("|");
+                }
+                Console.WriteLine(" ");
+            }
 
+        }
     }
 }
 
