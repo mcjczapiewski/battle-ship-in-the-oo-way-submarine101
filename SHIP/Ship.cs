494d93d14fb2057862bf3fb53436790fc4293c0e
igorn <i.nowacki89@gmail.com>
2020-06-15 21:11:19 +0200

    Initial Ship

--- SHIP/Ship.cs
+++ SHIP/Ship.cs
@@ -1,7 +1,34 @@
-namespace DefaultNamespace
+using System;
+
+namespace battle_ship_in_the_oo_way_submarine101.SHIP
 {
     public class Ship
     {
-        
+        public ShipEnum ShipType;
+        public string Name;
+        public int Life;
+        public bool IsHorizontal;
+        //public coordinates
+        public bool IsSunk 
+        { 
+            get 
+            {
+                return Life == 0;
+            }
+        }
+        public Ship(ShipEnum shipType, int life )
+        {
+            ShipType = shipType;
+            Life = life;
+        }
+        public void PlaceShipHorizontal()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PlaceShipVertical()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
