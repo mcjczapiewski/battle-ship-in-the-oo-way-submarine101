da5801e4a0dd1ffa47cabe81fb3168d6f2f44044
Maciej Czapiewski <mcjczapiewski@gmail.com>
2020-06-16 16:38:12 +0200

    First try to Square methods

--- OCEAN/Ocean.cs
+++ OCEAN/Ocean.cs
@@ -1,7 +1,9 @@
-namespace DefaultNamespace
+using System.Collections.Generic;
+
+namespace battle_ship_in_the_oo_way_submarine101.OCEAN
 {
     public class Ocean
     {
-        
+        List<List<string>> tableName;
     }
 }
--- SQUARE/Square.cs
+++ SQUARE/Square.cs
@@ -1,7 +1,49 @@
-namespace DefaultNamespace
+using battle_ship_in_the_oo_way_submarine101.OCEAN;
+
+namespace battle_ship_in_the_oo_way_submarine101.SQUARE
 {
     public class Square
     {
-        
+        public int CoordX;
+        public int CoordY;
+        public string Sign;
+        public bool AlreadyShooted;
+        public bool IsItFree;
+
+        public Square(int coordX,
+                      int coordY,
+                      string sign = " ",
+                      bool alreadyShooted = false,
+                      bool isItFree = true)
+        {
+            CoordX = coordX;
+            CoordY = coordY;
+            Sign = sign;
+            AlreadyShooted = alreadyShooted;
+            IsItFree = isItFree;
+        }
+
+        public void UpdateOccupationToShip(int coordX,
+                                int coordY,
+                                string tableName)
+        {
+            Square square = Ocean.tableName[coordX][coordY];
+            square.Sign = "S"; // S for Ship
+        }
+
+        public void Shoot(int coordX,
+                          int coordY,
+                          string tableName)
+        {
+            Square square = Ocean.tableName[coordX][coordY];
+            if (square.Sign == "S")
+            {
+                square.Sign = "X";
+            }
+            else
+            {
+                square.Sign = "O";
+            }
+        }
     }
 }
