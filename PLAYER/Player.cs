4fcfaf84770e7dea8d1859c5ae4c9e30af2bfaed
igorn <i.nowacki89@gmail.com>
2020-06-17 23:04:11 +0200

    Change Ship Concept and first version ship place

--- PLAYER/Player.cs
+++ PLAYER/Player.cs
@@ -1,5 +1,5 @@
 using System;
-
+using battle_ship_in_the_oo_way_submarine101.SHIP;
 
 namespace battle_ship_in_the_oo_way_submarine101.PLAYER
 {
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using battle_ship_in_the_oo_way_submarine101.OCEAN;
 using battle_ship_in_the_oo_way_submarine101.SQUARE;
+using battle_ship_in_the_oo_way_submarine101.SHIP;
 using System;
 
 namespace battle_ship_in_the_oo_way_submarine101
@@ -9,9 +10,12 @@ namespace battle_ship_in_the_oo_way_submarine101
         static void Main(string[] args)
         {
             Ocean ocean = new Ocean("XD");
+            Ship ship1 = new Ship("Destroyer", 2);
             Ocean.PrintBoard();
-            Square.UpdateOccupationToShip(2, 2);
+            //Square.UpdateOccupationToShip(2, 2);
+            Ship.PlaceShip(1, 2, ship1.Life, true);
             Ocean.PrintBoard();
+            Console.Read();
             Square.Shoot(2, 3);
             Ocean.PrintBoard();
         }
