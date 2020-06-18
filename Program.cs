37bc2f541d2d3a579564d5b2a681d39b7d49d9e6
Pati <ppeczynska@gmail.com>
2020-06-18 23:56:54 +0200

    try to print as non void method

--- Program.cs
+++ Program.cs
@@ -11,17 +11,10 @@ namespace battle_ship_in_the_oo_way_submarine101
         private static void Main()
         {
             Ocean ocean = new Ocean("XD");
-            Ship ship1 = new Ship(ShipEnum.Destroyer, 2);
-            Ocean.PrintBoard();
-            //Square.UpdateOccupationToShip(2, 2);
-            Ship.PlaceShip(1, 2, ship1.Life, true);
 
-            Ocean.PrintBoard();
-            Console.Read();
-            Square.Shoot(2, 3);
-
-            Ocean.PrintBoard();
-            PLAYER.Player.GetTheCoords();
+            Console.WriteLine(Ocean.NewMethodPrinting(ocean));
+            
+            
 
         }
     }
