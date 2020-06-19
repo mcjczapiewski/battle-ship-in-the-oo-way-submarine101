ff4dcf530d13271f3d5d35510819793a60682585
igorn <i.nowacki89@gmail.com>
2020-06-19 18:35:54 +0200

    Working version 1

--- OCEAN/Ocean.cs
+++ OCEAN/Ocean.cs
@@ -8,82 +8,133 @@ namespace battle_ship_in_the_oo_way_submarine101.OCEAN
     public class Ocean
     {
         private string Name;
-        private static int CoordX;
-        private static int CoordY;
-        public static Square[,] arrayOfSquares = new Square[10, 10];
-        
+        static int CoordX;
+        static int CoordY;
 
+        public static Square[,] arrayOfSquares = new Square[10, 10];
+        public static Square[,] arrayOfSq = new Square[10, 10];
         public Ocean(string name)
         {
             if (name != null) Name = name;
 
             for (CoordX = 0; CoordX < 10; CoordX++)
-            for (CoordY = 0; CoordY < 10; CoordY++)
-                arrayOfSquares[CoordX, CoordY] = new Square(CoordX, CoordY);
+            {
+                for (CoordY = 0; CoordY < 10; CoordY++)
+                {
+                    arrayOfSquares[CoordX, CoordY] = new Square(CoordX, CoordY);
+                }
+            }
         }
-        Ocean EnemyBoard = new Ocean("Enemy Board");
-        Ocean MyBoard = new Ocean("My Board");
-        
-        
-        public static void PrintBoard(string name)
+
+        //Ocean ocean = new Ocean("XD");
+        public static void PrintBoard()
         {
-            Ocean ocean = new Ocean("Board");
-            Console.WriteLine(ocean.Name);
-            Console.WriteLine("  A  B  C  D  E  F  G  H  I  J ");
+            //Square square = new Square(CoordX, CoordY);
+
+            //Console.WriteLine(ocean.Name);
+            Console.WriteLine("   A B C D E F G H I J");
             for (CoordY = 0; CoordY < 10; CoordY++)
             {
                 Console.Write(CoordY + 1);
-
-                if (CoordY != 9) Console.Write(" ");
+                if (CoordY != 9)
+                {
+                    Console.Write(" ");
+                }
 
                 for (CoordX = 0; CoordX < 10; CoordX++)
                 {
+
                     Console.Write(" " + arrayOfSquares[CoordX, CoordY].Sign);
+
                 }
                 Console.WriteLine();
             }
 
             //Console.WriteLine(square.Sign);
         }
-        //
-        // public static string UpperRow(Ocean ocean)
-        // {
-        //     ocean = new Ocean("Board");
-        //     var firstRow = "   A  B  C  D  E  F  G  H  I  J ";
-        //
-        //     return firstRow + Body();
-        // }
-        //
-        // private static string Body()
-        // {
-        //     for (CoordY = 0; CoordY < 10; CoordY++)
-        //     {
-        //         Console.Write("    " + CoordY + 1);
-        //         
-        //         for (CoordX = 0; CoordX < 10; CoordX++)
-        //         {
-        //             if (CoordY != 9)
-        //             {
-        //                 Console.WriteLine("    ");
-        //             }
-        //
-        //             Console.Write(" " + arrayOfSquares[CoordX, CoordY].Sign);
-        //         }
-        //     }
-        //
-        //     return Body();
-        // }
-        //
-        // public bool IsItBorder()
-        // {
-        //     if (Ocean.arrayOfSquares != null && (Ocean.arrayOfSquares(CoordX) < 0 && Ocean.arrayOfSquares(CoordX) > 10)
-        //     {
-        //         
-        //     }
-        //         return false;
-        // }
-       
-    }
-    
 
+    }
 }
+//{
+//    public class Ocean
+//    {
+//        private string Name;
+//        private static int CoordX;
+//        private static int CoordY;
+//        public static Square[,] arrayOfSquares = new Square[10, 10];
+
+
+//        public Ocean(string name)
+//        {
+//            if (name != null) Name = name;
+
+//            for (CoordX = 0; CoordX < 10; CoordX++)
+//            for (CoordY = 0; CoordY < 10; CoordY++)
+//                arrayOfSquares[CoordX, CoordY] = new Square(CoordX, CoordY);
+//        }
+//        Ocean EnemyBoard = new Ocean("Enemy Board");
+//        Ocean MyBoard = new Ocean("My Board");
+
+
+//        public static void PrintBoard()
+//        {
+//            Ocean ocean = new Ocean("Board");
+//            Console.WriteLine(ocean.Name);
+//            Console.WriteLine("  A  B  C  D  E  F  G  H  I  J ");
+//            for (CoordY = 0; CoordY < 10; CoordY++)
+//            {
+//                Console.Write(CoordY + 1);
+
+//                if (CoordY != 9) Console.Write(" ");
+
+//                for (CoordX = 0; CoordX < 10; CoordX++)
+//                {
+//                    Console.Write(" " + arrayOfSquares[CoordX, CoordY].Sign);
+//                }
+//                Console.WriteLine();
+//            }
+
+//            //Console.WriteLine(square.Sign);
+//        }
+//        //
+//        // public static string UpperRow(Ocean ocean)
+//        // {
+//        //     ocean = new Ocean("Board");
+//        //     var firstRow = "   A  B  C  D  E  F  G  H  I  J ";
+//        //
+//        //     return firstRow + Body();
+//        // }
+//        //
+//        // private static string Body()
+//        // {
+//        //     for (CoordY = 0; CoordY < 10; CoordY++)
+//        //     {
+//        //         Console.Write("    " + CoordY + 1);
+//        //         
+//        //         for (CoordX = 0; CoordX < 10; CoordX++)
+//        //         {
+//        //             if (CoordY != 9)
+//        //             {
+//        //                 Console.WriteLine("    ");
+//        //             }
+//        //
+//        //             Console.Write(" " + arrayOfSquares[CoordX, CoordY].Sign);
+//        //         }
+//        //     }
+//        //
+//        //     return Body();
+//        // }
+//        //
+//        // public bool IsItBorder()
+//        // {
+//        //     if (Ocean.arrayOfSquares != null && (Ocean.arrayOfSquares(CoordX) < 0 && Ocean.arrayOfSquares(CoordX) > 10)
+//        //     {
+//        //         
+//        //     }
+//        //         return false;
+//        // }
+
+//    }
+
+
+//}
--- Program.cs
+++ Program.cs
@@ -1,23 +1,41 @@
 ﻿using battle_ship_in_the_oo_way_submarine101.OCEAN;
 using battle_ship_in_the_oo_way_submarine101.PLAYER;
+using battle_ship_in_the_oo_way_submarine101.SHIP;
 using battle_ship_in_the_oo_way_submarine101.SQUARE;
 using System;
 
 namespace battle_ship_in_the_oo_way_submarine101
 {
-    internal class Program
+    class Program
     {
-        private static void Main()
+        static void Main(string[] args)
         {
-            new Ocean("XD");
+            Ocean ocean = new Ocean("XD");
+            Ship ship1 = new Ship("Destroyer", 2);
             Ocean.PrintBoard();
-            Square.UpdateOccupationToShip(2, 2);
-            Player player1 = Player.CreateNewPlayer();
-            Player.PlayerMove(player1);
+            //Square.UpdateOccupationToShip(2, 2);
+            Ship.PlaceShip(1, 2, ship1.Life, true);
             Ocean.PrintBoard();
-            Player.PlayerMove(player1);
+            Console.Read();
+            Square.Shoot(2, 3);
             Ocean.PrintBoard();
-            Console.ReadKey();
         }
     }
 }
+//{
+//    internal class Program
+//    {
+//        private static void Main()
+//        {
+//            new Ocean("XD");
+//            Ocean.PrintBoard("test");
+//            Square.UpdateOccupationToShip(2, 2);
+//            Player player1 = Player.CreateNewPlayer();
+//            Player.PlayerMove(player1);
+//            Ocean.PrintBoard("test");
+//            Player.PlayerMove(player1);
+//            Ocean.PrintBoard("test");
+//            Console.ReadKey();
+//        }
+//    }
+//}
