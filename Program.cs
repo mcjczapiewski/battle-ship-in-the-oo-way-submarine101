eb8b80dc78454a7616ba569a9a39c41d89234afd
Maciej Czapiewski <mcjczapiewski@gmail.com>
2020-06-20 15:09:43 +0200

    Copy before Igor merge :D

--- Program.cs
+++ Program.cs
@@ -1,20 +1,11 @@
-﻿using battle_ship_in_the_oo_way_submarine101.OCEAN;
-using battle_ship_in_the_oo_way_submarine101.PLAYER;
-using battle_ship_in_the_oo_way_submarine101.SHIP;
-using battle_ship_in_the_oo_way_submarine101.SQUARE;
-using System;
-using static battle_ship_in_the_oo_way_submarine101.MainLogic;
-
-namespace battle_ship_in_the_oo_way_submarine101
+﻿namespace battle_ship_in_the_oo_way_submarine101
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            
             MainLogic.Logic();
             MainLogic.AsciiArt();
-
         }
     }
 }
