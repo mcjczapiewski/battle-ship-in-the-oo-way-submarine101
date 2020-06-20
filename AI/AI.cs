12d5aeac4ac8e2c7558c31f45326f992da7a7a91
Pati <ppeczynska@gmail.com>
2020-06-20 22:30:08 +0200

    added empty AI

---
+++ AI/AI.cs
@@ -0,0 +1,25 @@
+using System;
+using battle_ship_in_the_oo_way_submarine101.SHIP;
+using battle_ship_in_the_oo_way_submarine101.PLAYER;
+using battle_ship_in_the_oo_way_submarine101.OCEAN;
+using battle_ship_in_the_oo_way_submarine101.SQUARE;
+
+namespace battle_ship_in_the_oo_way_submarine101.AI
+
+{
+    public class AI
+    {
+        public AI()
+        {
+            
+
+
+            // "D" => new Ship("Destroyer", 2, "D ", false),
+            // "C" => new Ship("Cruiser", 3, "C ", false),
+            // "S" => new Ship("Submarine", 3, "S ", false),
+            // "B" => new Ship("Battleship", 4, "B ", false),
+            // "R" => new Ship("Carrier", 5, "R ", false)
+            //
+        }
+    }
+}
