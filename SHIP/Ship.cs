2b1e709d21705f6a0e27765817f23cd8a9f59333
Maciej Czapiewski <mcjczapiewski@gmail.com>
2020-06-21 11:57:53 +0200

    Cleanup, don't do AI vs AI

--- SHIP/Ship.cs
+++ SHIP/Ship.cs
@@ -151,7 +151,7 @@ namespace battle_ship_in_the_oo_way_submarine101.SHIP
                     }
                 }
             }
-            return false;
+            return true;
         }
     }
 }
